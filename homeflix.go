3d82a7dd5523fd9f853b06a8511cd1e96c181cfa
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2021-10-04 19:38:51 -0500

    added custom path handler

--- homeflix.go
+++ homeflix.go
@@ -3,13 +3,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
+	"strings"
 )
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	in := r.URL.Path
+	in = strings.TrimPrefix(in, "/")
+	out := fmt.Sprintf("hello %s\n", in)
+	w.Write([]byte(out))
+}
 
 func main() {
 	var port = flag.String("port", "8080", "Define what TCP port to bind to")
-	var root = flag.String("root", ".", "Define the root filesystem path")
+	//var root = flag.String("root", ".", "Define the root filesystem path")
 
 	flag.Parse()
-	panic(http.ListenAndServe("0.0.0.0:"+*port, http.FileServer(http.Dir(*root))))
+
+	address := "0.0.0.0:" + *port
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(address, nil); err != nil {
+		panic(err)
+	}
 }
