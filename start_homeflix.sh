77c5e893d9021b5e6c0e8d774c1e9acd241449b5
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2023-02-23 16:35:23 -0600

    updated to rpi4

--- start_homeflix.sh
+++ start_homeflix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sshfs kyle@192.168.0.189:/home/kyle/data/movies /mnt/hdd/movies-rpi4/
+# sshfs kyle@192.168.0.189:/home/kyle/data/movies /mnt/hdd/movies-rpi4/
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-nohup homeflix /mnt/hdd/movies /mnt/hdd/movies-rpi4 &> log.log &
+# nohup homeflix /mnt/hdd/movies /mnt/hdd/movies-rpi4 &> log.log &
+nohup homeflix /mnt/hdd/movies /mnt/hdd-old/movies &> log.log &
